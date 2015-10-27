2a65915d84b78c449ff24746688374ecef6d7291
Eva <xueyike@gmail.com>
2015-10-27 16:20:21 -0400

    Add navigation button on Map
    keep tracking user’s current location
    
    Tap the button to start navigation in default map tool

--- DesiVLG/DesiVLG/ViewController.m
+++ DesiVLG/DesiVLG/ViewController.m
@@ -11,10 +11,11 @@
 #import "AppDelegate.h"
 
 
-@interface ViewController () <AddressDelegate>
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate, AddressDelegate>
 @property (nonatomic,retain) CLLocationManager* locationManager;
 @property (nonatomic,strong) CLGeocoder *geocoder;
 @property (nonatomic) bool delivery;
+@property (retain, nonatomic) MyPin *curSelectedPin;
 @property (weak, nonatomic) IBOutlet UILabel *orderTimeLabel;
 
 
@@ -30,8 +31,22 @@
     [self.deliveryBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
     self.pickUpBtn.enabled = NO;
 //    self.deliveryBtn.enabled = YES;
+    
+    self.mapView.delegate=self;
+    //request for location service
+    _locationManager=[[CLLocationManager alloc]init];
+    _locationManager.delegate = self;
+    if(![CLLocationManager locationServicesEnabled]||[CLLocationManager authorizationStatus]!=kCLAuthorizationStatusAuthorizedWhenInUse){
+        [_locationManager requestWhenInUseAuthorization];
+        
+    }
+    [_locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    _locationManager.delegate = self;
+    [_locationManager startUpdatingLocation];
+    
     self.annotations = [[NSMutableArray alloc] initWithCapacity:10];
     // Do any additional setup after loading the view from its nib.
+    
     _geocoder=[[CLGeocoder alloc]init];
     [self location];
 
@@ -110,7 +125,7 @@
     annotation.title=@"Desi Village Indian Restaurant";
     annotation.subtitle=@"4527 Baltimore Ave, Philadelphia, PA 19143";
     annotation.coordinate=location;
-    
+    self.curSelectedPin = annotation;
     //    float maxLat,minLat;
     //    float maxLong,minLong;
     //    if(_locationManager.location.coordinate.latitude > location.latitude){
@@ -142,6 +157,97 @@
     [self.mapView setRegion:region animated:YES];
     [self.mapView addAnnotation:annotation];
     [self.mapView selectAnnotation:annotation animated:YES];
+    
+}
+
+//delegate method of CLLocationManagerDelegate
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    NSLog(@"didChangeAuthorizationStatus---%u",status);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"didChangeAuthorizationStatusError----%@",error);
+}
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    
+    NSLog(@"%@",userLocation);
+    //    mapView.centerCoordinate = userLocation.location.coordinate;
+    //show the region range
+    //        MKCoordinateSpan span=MKCoordinateSpanMake(0.01, 0.01);
+    //        MKCoordinateRegion region=MKCoordinateRegionMake(userLocation.location.coordinate, span);
+    //        [_mapView setRegion:region animated:true];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView
+            viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    // If it's the user location, just return nil.
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    
+    // Handle any custom annotations.
+    if ([annotation isKindOfClass:[MyPin class]])
+    {
+        // Try to dequeue an existing pin view first.
+        MKPinAnnotationView*   pinView = (MKPinAnnotationView*)[mapView
+                                                                dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+        
+        if (!pinView)
+        {
+            // If an existing pin view was not available, create one.
+            pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                                      reuseIdentifier:@"CustomPinAnnotation"];
+            pinView.pinTintColor = [UIColor greenColor];
+            pinView.animatesDrop = YES;
+            pinView.canShowCallout = YES;
+            
+            //CLLocationCoordinate2D location =self.destination.coordinate;
+            // Add a detail disclosure button to the callout.
+            UIButton* rightButton = [UIButton buttonWithType:
+                                     UIButtonTypeRoundedRect];
+            [rightButton setFrame:(CGRect){260,0,pinView.frame.size.height*0.8,pinView.frame.size.height*0.8}];
+            [rightButton setBackgroundImage:[UIImage imageNamed:@"navi.png"] forState:UIControlStateNormal];
+            [rightButton addTarget:self action:@selector(pinButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
+            pinView.rightCalloutAccessoryView = rightButton;
+        }
+        else
+            pinView.annotation = annotation;
+        
+        return pinView;
+    }
+    
+    return nil;
+}
+
+-(void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKPinAnnotationView *)view{
+    self.curSelectedPin = view.annotation;
+}
+
+
+-(void)pinButtonTapped:(id)sender
+{
+    //AnnotationViewController *ann1 =[sender superview];
+    //NSString *str = ann1.title;
+    MyPin *ann = self.curSelectedPin;
+    CLLocationCoordinate2D to =[ann coordinate];
+    CLLocationCoordinate2D from = _locationManager.location.coordinate;
+    NSLog(@"Go navigation!");
+    MKMapItem *currentLocation = [[MKMapItem alloc] initWithPlacemark:[[MKPlacemark alloc] initWithCoordinate:from addressDictionary:nil]];
+
+    MKMapItem *toLocation = [[MKMapItem alloc] initWithPlacemark:[[MKPlacemark alloc] initWithCoordinate:to addressDictionary:nil]];
+    currentLocation.name = @"current location";
+    toLocation.name = ann.title;
+    NSArray *items = [NSArray arrayWithObjects:currentLocation, toLocation, nil];
+    
+    NSDictionary *options = @{
+                              MKLaunchOptionsDirectionsModeKey:MKLaunchOptionsDirectionsModeDriving,
+                              MKLaunchOptionsMapTypeKey:
+                                  [NSNumber numberWithInteger:MKMapTypeStandard],
+                              MKLaunchOptionsShowsTrafficKey:@YES
+                              };
+    //Using apple map
+    [MKMapItem openMapsWithItems:items launchOptions:options];
     
 }
 
